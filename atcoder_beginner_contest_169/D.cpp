da92fe3f6cc31d062a390387b62cfe9e164c1b7d
yongkim <penubokim@gmail.com>
2020-06-01 11:15:32 +0900

    beginner contest 169 D, E solved

--- atcoder_beginner_contest_169/D.cpp
+++ atcoder_beginner_contest_169/D.cpp
@@ -6,5 +6,34 @@ int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	long long n;
+	cin >> n;
+
+	vector<pair<long long, long long>> arr;
+	
+	for (long long i = 2; i * i <= n; ++i) {
+		if (n % i == 0) {
+			long long cnt = 0;
+			while (n % i == 0) {
+				n /= i;
+				cnt++;
+			}
+			arr.push_back({i, cnt});
+		}
+	}
+	if (n > 1) {
+		arr.push_back({n, 1});
+	}
+
+	int ret = 0;
+	for (auto a : arr) {
+		long long cnt = 0, sum = 0;
+		for (int i = 1; sum + i <= a.second; ++i) {
+			sum += i;
+			++cnt;
+		}
+		ret += cnt;
+	}
+	cout << ret << "\n";
 
 }
--- atcoder_beginner_contest_169/E.cpp
+++ atcoder_beginner_contest_169/E.cpp
@@ -1,10 +1,31 @@
 #include <bits/stdc++.h>
 
+
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
+	int n;
+	cin >> n;
+
+	vector<long long> a(n), b(n);
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		a[i] = x; b[i] = y;
+	}
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+
+	long long ret;
+
+	if (n % 2 == 1) {
+		ret = b[n / 2] - a[n / 2] + 1;
+	} else {
+		ret = b[n / 2 - 1] + b[n / 2] - (a[n / 2 - 1] + a[n / 2]) + 1;
+	}
+	cout << ret << "\n";
 
 }
